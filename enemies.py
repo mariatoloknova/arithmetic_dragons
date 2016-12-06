a982370208c1a9200a2117558510925889c0c31b
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-12-06 17:48:47 +0300

    new

--- enemies.py
+++ enemies.py
@@ -29,7 +29,7 @@ class Dragon(Enemy):
 class GreenDragon(Dragon):
     def __init__(self):
         self._health = 200
-        self._attack = 10
+        self._attack = randint(5,15)
         self._color = 'зелёный'
 
     def question(self):
@@ -39,8 +39,45 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
-# красный дракон учит вычитанию, а чёрный -- умножению.
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = randint(10,20)
+        self._color = 'красный'
+
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = randint(15,35)
+        self._color = 'черный'
+
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + 'x' + str(y)
+        self.set_answer(x * y)
+        return self.__quest
 
 
-enemy_types = [GreenDragon, RedDragon, BlackDragon]
+class MythrilDragon(Dragon):
+    def __init__(self):
+        self._health = 50
+        self._attack = randint(5,10)
+        self._color = 'мифриловый'
+
+    def question(self):
+        x = randint(0,10)
+        self.__quest = 'guess number in range (0;10)'
+        self.set_answer(x)
+        return self.__quest
+
+
+
+enemy_types = [GreenDragon, RedDragon, BlackDragon, MythrilDragon]
